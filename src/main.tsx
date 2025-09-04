6e2bb74007850b6265cac4ac94b3f5b99fda2214
aru456 <saiyanamadala456@gmail.com>
2025-09-04 10:28:18 -0400

    Fetch-abort-controller

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import PostList from './PostList.tsx'
+import Providers from './components/ThemeProviders.tsx'
+import './index.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Providers>
+      <PostList />
+    </Providers>
   </StrictMode>,
 )
